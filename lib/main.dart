f5c9191f9bafd0c7265671553e0d73bec181d93a
Ronit Kundnani <ronitkundnani08@gmail.com>
2025-07-04 13:53:13 +0530

    Authentication saved

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-// main.dart
 import 'package:chat_app/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/welcome_screens.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
@@ -12,7 +13,7 @@ import 'screens/new_contact_screen.dart';
 import 'screens/your_profile_screen.dart';
 import 'screens/call_screen.dart';
 import 'utils/app_theme.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -35,9 +36,8 @@ class SamParkApp extends StatelessWidget {
       title: 'SamPark',
       theme: AppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      home: const AuthGate(), // Set AuthGate as the home widget
       routes: {
-        '/': (context) => const WelcomeScreen(),
         '/login': (context) => const LoginScreen(),
         '/sign-up': (context) => const SignUpScreen(),
         '/home': (context) => const HomeScreen(),
@@ -47,6 +47,44 @@ class SamParkApp extends StatelessWidget {
         '/new-contact': (context) => const NewContactScreen(),
         '/your-profile': (context) => const YourProfileScreen(),
         '/call': (context) => const CallScreen(),
+      },
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            body: const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Optional: Add app logo (uncomment if you have an asset)
+                  // Image.asset('assets/logo.png', height: 100),
+                  // SizedBox(height: 20),
+                  CircularProgressIndicator(),
+                ],
+              ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            body: Center(child: Text('Error: ${snapshot.error}')),
+          );
+        } else if (snapshot.hasData) {
+          return const HomeScreen();
+        } else {
+          return const LoginScreen();
+        }
       },
     );
   }
